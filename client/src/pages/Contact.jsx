8a8a3e27151f0f16abcbd0bb7b7ecc0528953407
ajayarumugam73@gmail.com <you@example.com>
2025-09-14 08:17:06 +0530

    minor changes maded

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -15,8 +15,8 @@ const Contact = () => {
         <img src={assets.contact_img} alt="" className='w-full md:max-w-[480px]' />
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-xl text-gray-600'>Our Store</p>
-          <p className='text-gray-500'>54709 Williams Station <br /> Suite 350 Washington , USA</p>
-          <p className='text-gray-500'>Tell : (+91 9566381015) <br /> Email : djalok93068@gmail.com</p>
+          <p className='text-gray-500'>3/6C, Munaiyaduvarv Nayanar Street<br /> Palayamkottai, Tirunelveli - 627002</p>
+          <p className='text-gray-500'>Tell : (+91 9943066659) <br /> Email : greentrendztvl@gmail.com</p>
           <p className='font-semibold text-xl text-gray-600'>Careers at Forever</p>
           <p className='text-gray-500'>Learn More About or Teams and Job Opening</p>
           <button onClick={() => toast.error('Vaccancies Already Filled', { icon: '😅' })} className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
